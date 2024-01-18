491ec0b560d101b3ec7bb3235a271109ed9a8c55
Nitesh <nitesh.kumar1@faircent.com>
2024-01-18 16:28:52 +0530

    order file created and morgan added

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -3,22 +3,34 @@ import { connectDB } from "./utils/features.js";
 import { ErrorMiddleware } from "./middlewares/error.js";
 import userRoutes from "./routes/user.js";   //importing user routes
 import productRoutes from "./routes/product.js";   //importing product routes
+import orderRoutes from "./routes/order.js";   //importing product routes
 import NodeCache from "node-cache";   // it also stores data in ram as reddish did
+import { config } from "dotenv";    // dotenv configuration
+import morgan from "morgan";
+
+
+config({
+    path: "./.env"
+})
 
 const app = express();
-const port =3000;
-
+const port =process.env.PORT || 3000;
+const mongoURI = process.env.MONGO_DB_PARAMETERS || "";
 // mongo connection
 
-connectDB();
+connectDB(mongoURI);
 
 export const myCache = new NodeCache();
 
+// to take the request in the form of json
 app.use(express.json());
+
+// middle where which gives the api name which hit and req type and time takes to exe
+app.use(morgan("dev"));
 
 // health checking routes
 app.get('/ecommerce_health', (req,res,next) => {
-    res.send("Backend Working Fine");
+    res.send("Backend Working Fine with health of 100%");
 });
 
 app.get("/",(req, res, next)=>{
@@ -34,9 +46,20 @@ app.use('/api/v1/user', userRoutes);
 // using routes -> product route
 app.use('/api/v1/product', productRoutes);
 
+// using routes -> order route
+app.use('/api/v1/order', orderRoutes);
+
 app.use('/uploads', express.static("uploads"));
 app.use(ErrorMiddleware);
 
+// server on
 app.listen(port, ()=>{
     console.log(`Server is working on http://localhost:${port}`);
 })
+
+// handling when server got crashed
+process.on('uncaughtException', function (err) {
+    console.error(err);
+    console.log("Node Not Existing.....");
+    // console.log("Node Not Exiting...", err.message.split(":")[1].toUpperCase());
+});
---
+++ ecommerce-backend/src/controllers/order.ts
@@ -0,0 +1,11 @@
+import { TryCatch } from './../middlewares/error.js';
+import ErrorHandler from '../utils/utility-class.js';
+import { NextFunction, Request, Response } from 'express';
+import { myCache } from '../app.js';
+import { invalidatesCache } from '../utils/features.js';
+
+
+
+export const newOrder = ()=>{
+    
+}
