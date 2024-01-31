06685b8d05ea83dfd305d26565538f5a4fd28c2f
Nitesh <nitesh.kumar1@faircent.com>
2024-01-31 12:06:02 +0530

    authentication complete

---
+++ ecommerce-frontend/src/components/protected-route.tsx
@@ -0,0 +1,25 @@
+import { ReactElement } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+interface Props{
+  children?: ReactElement;
+  isAuthenticated : boolean;
+  adminRoute?: boolean;
+  isAdmin?: boolean;
+  redirect?: string;
+}
+
+const ProtectedRoute = ({children, isAuthenticated, adminRoute, isAdmin, redirect='/'}: Props) => {
+  if(!isAuthenticated){
+    return <Navigate to={redirect} />
+  }
+
+  if(adminRoute && !isAdmin){
+    return <Navigate to={redirect} /> 
+  }
+  return (
+    children ? children : <Outlet />
+  );
+};
+
+export default ProtectedRoute;
